cda23fd0d0146553767cf81e65b30461ad734422
Zoyac74567 <kittujie@gmail.com>
2025-08-06 18:50:09 +0530

    Add files via upload

--- ShrutiMusic/platforms/Resso.py
+++ ShrutiMusic/platforms/Resso.py
@@ -74,12 +74,3 @@ class RessoAPI:
             "thumb": thumbnail,
         }
         return track_details, vidid
-
-
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
